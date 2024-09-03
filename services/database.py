864e184cf75672801cdb0b5ab63b6230238974dc
Diego Balestra <diegobalestra9@gmail.com>
2024-09-03 22:49:12 +0000

    update database.py -> migrated to firebase database

--- services/database.py
+++ services/database.py
@@ -1,26 +1,33 @@
 from datetime import date
 
-from tinydb import Query, TinyDB
+import pyrebase
+import pyrebase.pyrebase as pb
 
+from config import DB_CONFIG
 from models import Member
-
-query = Query()
 
 
 class Database:
-    __db = TinyDB("database.json")
-    members_table = __db.table("members")
-    former_members_table = __db.table("former_members")
+    __db = pyrebase.initialize_app(DB_CONFIG).database()
+
+    @property
+    def __members(self) -> pb.Database:
+        return self.__db.child("members")
+
+    @property
+    def __former_members(self) -> pb.Database:
+        return self.__db.child("former_members")
 
     def get_members(self) -> list[Member]:
         return sorted(
-            [Member.from_dict(m) for m in self.members_table.all()],
+            [Member.from_dict(m.val()) for m in self.__members.get()],
             key=lambda m: m.trophies,
             reverse=True,
         )
 
     def save_member(self, member: Member) -> None:
-        existing_member_dict = self.members_table.get(query.tag == member.tag)
+        tag = member.tag.strip("#")
+        existing_member_dict = self.__members.child(tag).get().val()
 
         if existing_member_dict:
             existing_member = Member.from_dict(existing_member_dict)
@@ -32,9 +39,7 @@ class Database:
                     member.real_name if member.real_name else existing_member.real_name
                 ),
                 "trophies": member.trophies,
-                "strikes": (
-                    member.strikes if member.strikes else existing_member.strikes
-                ),
+                "strikes": member.strikes,
                 "birthday": (
                     member.birthday.isoformat()
                     if member.birthday
@@ -52,15 +57,17 @@ class Database:
                 "club": member.club.to_dict(),
             }
 
-            self.members_table.update(updated_member_dict, query.tag == member.tag)
+            self.__members.child(tag).update(updated_member_dict)
         else:
-            self.members_table.insert(member.to_dict())
+            self.__members.child(tag).set(member.to_dict())
 
     def remove_member(self, member: Member) -> None:
-        self.members_table.remove(query.tag == member.tag)
+        tag = member.tag.strip("#")
+        self.__members.child(tag).remove()
 
     def add_former_member(self, member: Member) -> None:
+        tag = member.tag.strip("#")
         member_dict = member.to_dict()
         member_dict["departure_date"] = date.today().isoformat()
         member_dict["notes"] = None
-        self.former_members_table.insert(member_dict)
+        self.__former_members.child(tag).set(member_dict)
