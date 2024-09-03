d798b4d3477f1098e9455d34091d052e4e217ed8
Diego Balestra <diegobalestra9@gmail.com>
2024-09-03 06:05:18 +0000

    feat: create whole_club.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,2 @@
 from .database import Database
+from .whole_club import WholeClub
---
+++ services/whole_club.py
@@ -0,0 +1,63 @@
+import os
+from typing import Optional
+
+import requests
+from dotenv import load_dotenv
+
+from models import Member
+
+from .database import Database
+
+load_dotenv()
+
+api_key = os.getenv("BRAWL_API_KEY")
+
+parse_code = lambda code: code.strip("#")
+
+
+class WholeClub:
+    def __init__(self, main: str, feeders: Optional[list[str]] = None) -> None:
+        self.main = main
+        self.feeders = feeders
+
+    @property
+    def members(self) -> list[Member]:
+        return Database().get_members()
+
+    @property
+    def trophies(self) -> int:
+        return sum(member.trophies for member in self.members)
+
+    def __fetch_members(self, code: str) -> list[Member]:
+        data = []
+        headers = {"Authorization": f"Bearer {api_key}"}
+        base_url = f"https://bsproxy.royaleapi.dev/v1/clubs"
+
+        code = parse_code(code)
+        response = requests.get(f"{base_url}/%23{code}", headers=headers)
+        if response.status_code == 200:
+            res_json = response.json()
+
+            for member in res_json["members"]:
+                member["club"] = res_json
+                data.append(Member.from_dict(member))
+
+        return data
+
+    def update_members(self) -> None:
+        db = Database()
+        current_members = self.__fetch_members(self.main)
+
+        if self.feeders:
+            for feeder in self.feeders:
+                current_members += self.__fetch_members(feeder)
+
+        saved_members = db.get_members()
+        former_members = [m for m in saved_members if m not in current_members]
+
+        for former_member in former_members:
+            db.add_former_member(former_member)
+            db.remove_member(former_member)
+
+        for member in current_members:
+            db.save_member(member)
