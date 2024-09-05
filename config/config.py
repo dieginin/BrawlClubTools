4b8de830a871a80969c55ed34e858dd4e1636e63
dieginin <diegobalestra9@gmail.com>
2024-09-04 21:18:49 -0400

    update config.py

--- config/config.py
+++ config/config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_KEY = os.getenv("BRAWL_KEY")
 DB_CONFIG = {
     "apiKey": os.getenv("DB_KEY"),
-    "authDomain": "brawlclubtools.firebaseapp.com",
-    "databaseURL": "https://brawlclubtools-default-rtdb.firebaseio.com",
-    "projectId": "brawlclubtools",
-    "storageBucket": "brawlclubtools.appspot.com",
-    "messagingSenderId": "876424576126",
-    "appId": "1:876424576126:web:1cc9546a112b4307b86c2f",
-    "measurementId": "G-4M0SP5H43L",
+    "authDomain": f"{os.getenv("PROJECTID")}.firebaseapp.com",
+    "databaseURL": os.getenv("DB_URL"),
+    "projectId": os.getenv("PROJECTID"),
+    "storageBucket": f"{os.getenv("PROJECTID")}.appspot.com",
+    "messagingSenderId": os.getenv("MSG_SNDR_ID"),
+    "appId": os.getenv("APPID"),
+    "measurementId": os.getenv("MSRMTID"),
 }
